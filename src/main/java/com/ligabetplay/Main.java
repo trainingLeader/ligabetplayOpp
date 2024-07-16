21eda7790e0f5917277357a98cb0d3671f665fd7
darksidePardo <jjpardo2002@gmail.com>
2024-07-16 13:03:45 -0500

    feat: :sparkles: Add new features
    Add features main

--- src/main/java/com/ligabetplay/Main.java
+++ src/main/java/com/ligabetplay/Main.java
@@ -1,28 +1,36 @@
 package com.ligabetplay;
 
-import java.util.Optional;
+import com.ligabetplay.country.application.SaveCountryUseCase;
+import com.ligabetplay.country.domain.service.CountryService;
+import com.ligabetplay.country.infrastructure.CountryRepository;
+import com.ligabetplay.country.infrastructure.CountryUI;
 
-import javax.swing.JOptionPane;
+// import java.util.Optional;
+
+// import javax.swing.JOptionPane;
 
 //import javax.swing.JOptionPane;
 
-import com.ligabetplay.person.application.CreatePersonUseCase;
-import com.ligabetplay.person.application.FindPersonCiudad;
-import com.ligabetplay.person.application.FindPersonUseCase;
-import com.ligabetplay.person.domain.entity.Person;
-import com.ligabetplay.person.domain.entity.PersonDto;
+// import com.ligabetplay.person.application.CreatePersonUseCase;
+// import com.ligabetplay.person.application.FindPersonCiudad;
+// import com.ligabetplay.person.application.FindPersonUseCase;
+// import com.ligabetplay.person.domain.entity.Person;
+// import com.ligabetplay.person.domain.entity.PersonDto;
 //import com.ligabetplay.person.domain.entity.PersonaDto;
-import com.ligabetplay.person.domain.service.PersonService;
-import com.ligabetplay.person.infrastructure.PersonRepository;
-import com.ligabetplay.person.infrastructure.PersonUi;
+// import com.ligabetplay.person.domain.service.PersonService;
+// import com.ligabetplay.person.infrastructure.PersonRepository;
+// import com.ligabetplay.person.infrastructure.PersonUi;
 
 public class Main {
     public static void main(String[] args) {
-        PersonService personService = new PersonRepository();
-        CreatePersonUseCase createPersonUseCase = new CreatePersonUseCase(personService);
-        FindPersonUseCase findPersonById = new FindPersonUseCase(personService); 
-        FindPersonCiudad fpc = new FindPersonCiudad(personService); 
-        PersonUi consoleAdapter = new PersonUi(createPersonUseCase, findPersonById,fpc);
+        CountryService cs = new CountryRepository();
+        SaveCountryUseCase scu = new SaveCountryUseCase(cs);
+        CountryUI uiCountry = new CountryUI(scu);
+        // PersonService personService = new PersonRepository();
+        // CreatePersonUseCase createPersonUseCase = new CreatePersonUseCase(personService);
+        // FindPersonUseCase findPersonById = new FindPersonUseCase(personService); 
+        // FindPersonCiudad fpc = new FindPersonCiudad(personService); 
+        // PersonUi consoleAdapter = new PersonUi(createPersonUseCase, findPersonById,fpc);
         // Optional<PersonDto> miPersona = consoleAdapter.findPersonCiudad();
 
         // miPersona.ifPresentOrElse(
@@ -37,7 +45,7 @@ public class Main {
         //             }
                 
         //         );
-        consoleAdapter.frmRegPerson();
+        uiCountry.frmRegPerson();
         
         // consoleAdapter.start();
     }
